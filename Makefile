a49893e5a81d2f4dc4f3fc6dc07dd576bf19742e
Eugene Sorokin <akakeevich@gmail.com>
2019-05-27 21:00:09 +0700

    Geometry

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
-geom.o: geom.c
-	gcc -Wall -c geom.c
-geom: geom.o
-	gcc -Wall -o geom geom.o
+err.o: err.c
+	gcc -Wall -c err.c
+main.o: main.c
+	gcc -Wall -c main.c
+geom: main.o err.o
+	gcc -Wall -o geom main.o err.o
 	rm -rf *.o
 	rm -rf *.c.save
 all: geom
---
+++ err.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float err(float x,float y,float r)
+{
+	if(x>=0 && y>=0 && r>=0)
+	{
+		return 0;
+	}
+	else if(x<=0 && y<=0 && r<=0)
+	{
+		return 0;	
+	}	
+	else{
+		printf("Введены неверные данные\n");
+		exit(0);
+	}
+}
